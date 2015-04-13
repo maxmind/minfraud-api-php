c5a6d124d63bd4fbd1b3141f5c290d5fecd2dd8d
Gregory Oschwald <goschwald@maxmind.com>
2015-04-13 11:09:15 -0700

    Added "with*" methods for setting inputs
    This is similar to how the value objects in the current PSR-7 draft
    work. We allow for setting it all at once in the constructor if
    anyone is worried about the very small overhead from creating multiple
    objects (or otherwise has a reason to construct the array ahead of
    time). Each object remains immutable.

--- src/MaxMind/MinFraud.php
+++ src/MaxMind/MinFraud.php
@@ -2,12 +2,7 @@
 
 namespace MaxMind;
 
-use MaxMind\Exception\AuthenticationException;
-use MaxMind\Exception\HttpException;
 use MaxMind\Exception\InvalidInputException;
-use MaxMind\Exception\InvalidRequestException;
-use MaxMind\Exception\MinFraudException;
-use MaxMind\Exception\OutOfCreditsException;
 use MaxMind\WebService\Client;
 
 /**
@@ -19,6 +14,7 @@ class MinFraud
 
     private $client;
     private $basePath = '/minfraud/v2.0/';
+    private $content;
 
     /**
      * @param int $userId
@@ -26,33 +22,114 @@ class MinFraud
      */
     public function __construct(
         $userId,
-        $licenseKey
+        $licenseKey,
+        $content = array()
     ) {
         $this->client = new Client($userId, $licenseKey);
+        $this->content = $content;
+    }
+
+    public function withEvent($values)
+    {
+        $new = clone $this;
+        $new->content['event'] = $values;
+        return $new;
+    }
+
+    public function withAccount($values)
+    {
+        $new = clone $this;
+        $new->content['account'] = $values;
+        return $new;
+    }
+
+    public function withEmail($values)
+    {
+        $new = clone $this;
+        $new->content['email'] = $values;
+        return $new;
+    }
+
+    public function withBilling($values)
+    {
+        $new = clone $this;
+        $new->content['billing'] = $values;
+        return $new;
+    }
+
+
+    public function withShipping($values)
+    {
+        $new = clone $this;
+        $new->content['shipping'] = $values;
+        return $new;
+    }
+
+    public function withPayment($values)
+    {
+        $new = clone $this;
+        $new->content['payment'] = $values;
+        return $new;
+    }
+
+    public function withCreditCard($values)
+    {
+        $new = clone $this;
+        $new->content['credit_card'] = $values;
+        return $new;
+    }
+
+    public function withOrder($values)
+    {
+        $new = clone $this;
+        $new->content['order'] = $values;
+        return $new;
+    }
+
+    public function withShoppingCartItem($values)
+    {
+        $new = clone $this;
+        if (!isset($new->content['shopping_cart'])) {
+            $new->content['shopping_cart'] = array();
+        }
+        array_push($new->content['shopping_cart'], $values);
+        return $new;
+    }
+
+    public function withDevice($values)
+    {
+        $new = clone $this;
+        $new->content['device'] = $values;
+        return $new;
     }
 
     /**
      * @param array $input
      * @return \MaxMind\MinFraud\Model\Score
      */
-    public function score($input)
+    public function score()
     {
-        return $this->post('Score', $input);
+        return $this->post('Score');
     }
 
     /**
      * @param array $input
      * @return \MaxMind\MinFraud\Model\Insights
      */
-    public function insights($input)
+    public function insights()
     {
-        return $this->post('Insights', $input);
+        return $this->post('Insights');
     }
 
-    private function post($service, $input)
+    private function post($service)
     {
+        if (!isset($this->content['device']['ip_address'])) {
+            throw new InvalidInputException(
+                'The device "ip_address" field is required'
+            );
+        }
         $url = $this->basePath . strtolower($service);
         $class = "MaxMind\\MinFraud\\Model\\" . $service;
-        return new $class($this->client->post($service, $url, $input));
+        return new $class($this->client->post($service, $url, $this->content));
     }
 }
