d0d65cc3ff42bb7fc8d228130cd70b617d00f071
Gregory Oschwald <goschwald@maxmind.com>
2015-10-13 15:15:26 -0700

    Update credit card property descriptions to be more accurate

--- src/MinFraud/Model/CreditCard.php
+++ src/MinFraud/Model/CreditCard.php
@@ -6,13 +6,17 @@ namespace MaxMind\MinFraud\Model;
  * Class CreditCard
  * @package MaxMind\MinFraud\Model
  *
- * @property string $country This property contains an {@link
+ * @property string $country This property contains the the two letter {@link
  * http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2 ISO 3166-1 alpha-2 country
- * code} representing the country that the card was issued in.
+ * code} associated with the location of the majority of customers using this
+ * credit card as determined by their billing address. In cases where the
+ * location of customers is highly mixed, this defaults to the country of the
+ * bank issuing the card.
  *
  * @property boolean $isIssuedInBillingAddressCountry This property is true if
- * the country of the billing address matches the country that the credit card
- * was issued in.
+ * the country of the billing address matches the country of the majority of
+ * customers using this credit card. In cases where the location of customers
+ * is highly mixed, the match is to the country of the bank issuing the card.
  *
  * @property boolean $isPrepaid This property is true if the card is a prepaid
  * card.
