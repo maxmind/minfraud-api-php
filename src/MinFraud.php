a3d42102e0b2176a946bae93119ae2c25e19e8cf
Gregory Oschwald <goschwald@maxmind.com>
2016-06-08 15:11:43 -0700

    Update for v0.6.0

--- src/MinFraud.php
+++ src/MinFraud.php
@@ -39,7 +39,7 @@ use Respect\Validation\Exceptions\ValidationException;
  */
 class MinFraud
 {
-    const VERSION = 'v0.5.0';
+    const VERSION = 'v0.6.0';
 
     private $client;
     private static $host = 'minfraud.maxmind.com';
