edc9098563758ba4c10f7a4348d905ff78fd06b4
William Storey <wstorey@maxmind.com>
2021-09-09 16:00:50 -0700

    Remove documentation for non existing disposition action

--- src/MinFraud/Model/Disposition.php
+++ src/MinFraud/Model/Disposition.php
@@ -14,8 +14,8 @@ namespace MaxMind\MinFraud\Model;
  * "manual_review", "reject", and "test". If you do not have custom rules set
  * up, `null` will be returned.
  * @property-read string|null $reason The reason for the action. The current
- * possible values are "custom_rule", "block_list", and "default". If you do
- * not have custom rules set up, `null` will be returned.
+ * possible values are "custom_rule" and "default". If you do not have custom
+ * rules set up, `null` will be returned.
  * @property-read string|null $ruleLabel The label of the custom rule that was
  * triggered. If you do not have custom rules set up, the triggered custom rule
  * does not have a label, or no custom rule was triggered, `null` will be
