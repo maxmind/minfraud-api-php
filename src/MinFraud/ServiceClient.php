47d3ef1c6bf0465158fa15d5b2843f3f6c6a4f8b
Gregory Oschwald <goschwald@maxmind.com>
2022-01-25 09:41:15 -0800

    Update for 1.20.0

--- src/MinFraud/ServiceClient.php
+++ src/MinFraud/ServiceClient.php
@@ -10,7 +10,7 @@ use Respect\Validation\Exceptions\ValidationException;
 
 abstract class ServiceClient
 {
-    public const VERSION = 'v1.19.0';
+    public const VERSION = 'v1.20.0';
 
     /**
      * @var Client
