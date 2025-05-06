d882f74a5ac88abe467fa950b0d4468927e8a6cc
Gregory Oschwald <goschwald@maxmind.com>
2025-05-06 10:46:00 -0700

    Set correct version in composer require

--- dev-bin/release.sh
+++ dev-bin/release.sh
@@ -45,6 +45,7 @@ php composer.phar self-update
 php composer.phar update --no-dev
 
 perl -pi -e "s/(?<=const VERSION = ').+?(?=';)/$tag/g" src/MinFraud/ServiceClient.php
+perl -pi -e "s{(?<=php composer\.phar require maxmind\/minfraud:).+}{^$version}g" README.md
 
 box_phar_hash='8d12a7d69a5003a80bd603ea95a8f3dcea30b9a2ad84cd7cb15b8193929def9e  box.phar'
 
